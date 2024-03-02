91729751f665ec25f515f2e4a1093f86a4ef5b33
Ankit Chaudhary <ankitcha810@gmail.com>
2024-03-02 13:17:46 +0530

    Recursion: firstPos lastPost printall , saveallPos

--- Recursion/3.cpp
+++ Recursion/3.cpp
@@ -11,14 +11,16 @@ bool isSorted(int arr[], int n)
 
     bool isSmallerSorted = isSorted(arr + 1, n - 1);
 
-    /*if (isSmallerSorted)
+    /*
+    if (isSmallerSorted)
     {
         return true;
     }
     else
     {
         return false;
-    }*/
+    }
+    */
 
     return isSmallerSorted;
 }
@@ -32,6 +34,80 @@ int sumOfArray(int arr[], int n)
     return arr[0] + sumOfArray(arr + 1, n - 1);
 }
 
+bool checkElement(int arr[], int n, int ele)
+{
+
+    if (n == 0)
+    {
+        return false;
+    }
+
+    if (ele == arr[0])
+        return true;
+
+    return checkElement(arr + 1, n - 1, ele);
+}
+
+// pending
+int FirstPos(int arr[], int n, int x, int i)
+{
+
+    if (i == n)
+    {
+        return -1;
+    }
+    if (arr[i] == x)
+    {
+        return i;
+    }
+
+    return FirstPos(arr, n, x, i + 1);
+}
+
+// pending
+int LastPost(int arr[], int n, int x, int i, int ans)
+{
+
+    if (i == n)
+    {
+        return ans;
+    }
+    if (arr[i] == x)
+    {
+        ans = i;
+    }
+    return LastPost(arr, n, x, i + 1, ans);
+}
+
+void PrintAllPos(int arr[], int n, int x, int i)
+{
+
+    if (i == n)
+    {
+        return;
+    }
+
+    if (arr[i] == x)
+    {
+        cout << i << " ";
+    }
+
+    PrintAllPos(arr, n, x, i + 1);
+}
+
+void saveAllPos(int arr[], int n, int x, int i, vector<int> &ans)
+{
+
+    if (i == n)
+    {
+        return;
+    }
+    if (arr[i] == x)
+        ans.push_back(i);
+
+    saveAllPos(arr, n, x, i + 1, ans);
+}
+
 int main()
 {
 
@@ -43,13 +119,37 @@ int main()
         cin >> arr[i];
     /*
     bool ans = isSorted(arr, n);
+
     cout << "ans: " << ans << '\n';
     if (ans)
         cout << "array is sorted" << '\n';
     else
         cout << "array is not Sorted" << '\n';
     */
-    cout << sumOfArray(arr, n) << '\n';
+
+    // cout << sumOfArray(arr, n) << '\n';
+
+    // if (checkElement(arr, n, 3))
+    // {
+    //     cout << "Element is there" << '\n';
+    // }
+    // else
+
+    //     cout << "Element is There" << '\n';
+
+    // cout << FirstPos(arr, n, 3, 0, 0) << '\n';
+
+    // cout << LastPost(arr, n, 3, 0, -1) << '\n';
+
+    // PrintAllPos(arr, n, 3, 0);
+
+    vector<int> ans;
+    saveAllPos(arr, n, 3, 0, ans);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
 
     return 0;
 }
